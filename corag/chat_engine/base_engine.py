997e9629996a05edaef4af651bc8081dfed3ca1e
aditya2901 <shanmugham.a@northeastern.edu>
2025-02-17 14:47:06 -0800

    added support to chatgpt

---
+++ corag/chat_engine/base_engine.py
@@ -0,0 +1,7 @@
+class BaseChatEngine:
+    def __init__(self, model_name):
+        print(f"[INFO] using the model : {model_name}")
+
+    def chat(self, chat_inputs: dict):
+        raise Exception("chat function is not implemented")
+    
--- corag/chat_engine/chatgpt.py
+++ corag/chat_engine/chatgpt.py
@@ -1,5 +1,79 @@
 from openai import OpenAI
+import requests
+import os
+
+from . import BaseChatEngine
+
+class GPT(BaseChatEngine):
+    def __init__(self, 
+                 model_name = "o3-mini",
+                 temperature = 0.7,
+                 max_tokens = 100):
+        super().__init__(model_name)
+        self._model_name = model_name
+        self._temperature = temperature
+        self._max_tokens = max_tokens
+
+        self.url = "https://api.openai.com/v1/chat/completions"
+        self.api_key = self.__load_api_key()
+        self.headers = {
+            "Authorization": f"Bearer {self.api_key}",
+            "Content-Type": "application/json"
+        }
 
 
-class GPT:
-    def __init__(self, model_name = )
+    def __load_api_key(self):
+        key = os.getenv("OPENAI_API_KEY", "NOT-FOUND")
+
+        if key == "NOT-FOUND":
+            raise Exception("OPENAI API KEY not found in env variables")
+        
+        return key
+    
+    def format_input(self, system: str, user: list, assistant: list):
+        output = []
+
+        if system is not None:
+            temp = {"role" : "system", "content" : system}
+            output.append(temp)
+
+        for i in range(len(user)):
+            if i < len(user):
+                temp = {"role" : "user", "content" : user[i]}
+                output.append(temp)
+            
+            if i < len(assistant):
+                temp = {"role" : "user", "content" : assistant[i]}
+                output.append(temp)
+        
+        return output
+
+    def chat(self, chat_inputs: dict, raw_output = False):
+        # retrieve the inputs
+        user_messages = chat_inputs.get("user", [])
+        system_message = chat_inputs.get("system", None)
+        assistant_messages = chat_inputs.get("assistant", [])
+
+        formatted_input = self.format_input(system = system_message,
+                                            user = user_messages,
+                                            assistant = assistant_messages)
+
+
+        data = {
+            "model": self._model_name,  
+            "messages": formatted_input,
+            "temperature": self._temperature, 
+            "max_tokens": self._max_tokens
+        }
+
+        response = requests.post(self.url, headers = self.headers, json = data)
+
+        if response.status_code == 200:
+            raw_result = response.json()
+            output = raw_result["choices"][0]["message"]["content"]
+
+        if raw_output:
+            return raw_result
+        return output
+
+
