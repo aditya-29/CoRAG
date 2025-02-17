1ac19163f21dbcf8993d7ab963610a8caef412c2
aditya2901 <shanmugham.a@northeastern.edu>
2025-02-17 14:23:55 -0800

    added support to store api keys

--- corag/__init__.py
+++ corag/__init__.py
@@ -1,1 +1,27 @@
 from .prompts import prompts as Prompts
+
+import os
+import yaml
+
+# export the keys
+def load_secrets(file_path = "../secrets.txt"):
+    if not os.path.exists(file_path):
+        print("[WARN] No secrets file found")
+    
+    with open(file_path, "r") as file:
+        secrets = yaml.safe_load(file)  # Safe load prevents arbitrary code execution
+
+    for key, value in secrets.get("api_keys", {}).items():
+        env_var_name = key.upper()
+        os.environ[env_var_name] = value
+
+        print(f"Exported {env_var_name} to environment variable")
+
+
+load_secrets()
+    
+    
+    
+    
+    
+    
---
+++ corag/chat_engine/chatgpt.py
@@ -0,0 +1,5 @@
+from openai import OpenAI
+
+
+class GPT:
+    def __init__(self, model_name = )
